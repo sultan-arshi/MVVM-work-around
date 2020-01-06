5bc2f0cc45fe94f654eade7e0245c9a0a2f12791
Sultan Ali <sultan.ali@securehops.com>
2020-01-06 13:38:58 +0500

    Initial Commit

---
+++ MVVM-Wrok-Around/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-Wrok-Around
+//
+//  Created by Sultan Ali on 2020-01-06.
+//  Copyright © 2020 Sultan Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVM-Wrok-AroundTests/MVVM_Wrok_AroundTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_Wrok_AroundTests.swift
+//  MVVM-Wrok-AroundTests
+//
+//  Created by Sultan Ali on 2020-01-06.
+//  Copyright © 2020 Sultan Ali. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_Wrok_Around
+
+class MVVM_Wrok_AroundTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
